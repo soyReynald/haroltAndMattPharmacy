d3a94d955b1e11f868308bb8168fda1d77eead2e
soyreynald <reynaldramirez@hotmail.com>
2024-07-13 09:52:53 -0400

    Updates in the task made yesterday before Sabbath

---
+++ db_includes/conexion.php
@@ -0,0 +1,38 @@
+<?php
+define("USER", "root");
+define("DB_NAME", "sample_pharmacy");
+define("PASSWORD", "");
+define("SERVERNAME", "localhost");
+
+
+$conn = new mysqli(SERVERNAME, USER, PASSWORD, DB_NAME);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+} else {
+	echo json_encode("Connected successfully");
+}
+/*
+TO INSERT:
+
+$sql = "INSERT INTO MyGuests (firstname, lastname, email)
+VALUES ('John', 'Doe', 'john@example.com')";
+
+*/
+
+/*
+TO UPDATE:
+
+$sql = "UPDATE MyGuests SET lastname='Doe' WHERE id=2";
+
+if ($conn->query($sql) === TRUE) {
+  echo "Record updated successfully";
+} else {
+  echo "Error updating record: " . $conn->error;
+}
+
+$conn->close();
+*/
+
+
+?>
